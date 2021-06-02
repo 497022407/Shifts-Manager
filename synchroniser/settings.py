beda2fa584f7c0ccd45ad9c0f1b1f712c8f59408
David Wang <c497022407@gmail.com>
2021-06-02 23:47:36 +0930

    set STATIC_ROOT and STATIC_URL

--- synchroniser/settings.py
+++ synchroniser/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
