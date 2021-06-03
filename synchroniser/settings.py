222325f0e64837057bf0c209c5f49adc183fb21d
David Wang <c497022407@gmail.com>
2021-06-03 13:49:16 +0930

    added extra places to find statics

--- synchroniser/settings.py
+++ synchroniser/settings.py
@@ -135,6 +135,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
