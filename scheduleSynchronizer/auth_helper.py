beccb63c8622c015a9a453f586d4c3bb5d5066b9
David Wang <h.wang@student.adelaide.edu.au>
2021-06-06 18:00:14 +0930

    fix sign-in timezone bug

--- scheduleSynchronizer/auth_helper.py
+++ scheduleSynchronizer/auth_helper.py
@@ -57,7 +57,7 @@ def store_user(request, user):
       'is_authenticated': True,
       'name': user['displayName'],
       'email': user['mail'] if (user['mail'] != None) else user['userPrincipalName'],
-      'timeZone': user['mailboxSettings']['timeZone']
+      'timeZone': 'Cen. Australia Standard Time'
     }
   except Exception as e:
     print(e)
